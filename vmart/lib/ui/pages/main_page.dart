73af8fb0177dcb3531eed14f0635d1f60736923f
aldi8477 <aldinurhanudin@gmail.com>
2022-07-12 23:08:44 +0700

    ProfilePage

--- vmart/lib/ui/pages/main_page.dart
+++ vmart/lib/ui/pages/main_page.dart
@@ -37,7 +37,7 @@ class _MainPageState extends State<MainPage> {
                 child: OrderHistoryPage(),
               ),
               Center(
-                child: Text("Profile"),
+                child: ProfilePage(),
               ),
             ],
           )),
--- vmart/lib/ui/pages/order_history_page.dart
+++ vmart/lib/ui/pages/order_history_page.dart
@@ -1,26 +1,28 @@
 part of 'pages.dart';
 
-class OrderHistoryPage extends StatelessWidget {
+class OrderHistoryPage extends StatefulWidget {
+  @override
+  _OrderHistoryPageState createState() => _OrderHistoryPageState();
+}
+
+class _OrderHistoryPageState extends State<OrderHistoryPage> {
   int selectedIndex = 0;
-  List<Transaction> inProgress =
-      // [];
-      mockTransaction
-          .where((element) =>
-              element.status == TransactionStatus.delivered ||
-              element.status == TransactionStatus.pending)
-          .toList();
-  List<Transaction> past =
-      // [];
-      mockTransaction
-          .where((element) =>
-              element.status == TransactionStatus.delivered ||
-              element.status == TransactionStatus.cancelled)
-          .toList();
+  List<Transaction> inProgress = mockTransaction
+      .where((element) =>
+          element.status == TransactionStatus.delivered ||
+          element.status == TransactionStatus.pending)
+      .toList();
+  List<Transaction> past = mockTransaction
+      .where((element) =>
+          element.status == TransactionStatus.delivered ||
+          element.status == TransactionStatus.cancelled)
+      .toList();
+
   @override
   Widget build(BuildContext context) {
     if (inProgress.length == 0 && past.length == 0) {
       return IllustrationPage(
-        title: 'ouch! Hungry',
+        title: 'Ouch! Hungry',
         subtitle: 'Seems you like have not\nordered any food yet',
         picturePath: 'assets/love_burger.png',
         buttonTap1: () {},
@@ -57,45 +59,43 @@ class OrderHistoryPage extends StatelessWidget {
                   ],
                 ),
               ),
-              //// body
+              //// Body
               Container(
-                  width: double.infinity,
-                  color: Colors.white,
-                  child: Column(
-                    children: [
-                      CustomTabBar(
-                        titles: ['In Progress', 'Past Orders'],
-                        selectedIndex: selectedIndex,
-                        onTap: (index) {
-                          setState(() {
-                            selectedIndex = index;
-                          });
-                        },
-                      ),
-                      SizedBox(
-                        height: 16,
-                      ),
-                      Column(
-                        children: (selectedIndex == 0 ? inProgress : past)
-                            .map((e) => Padding(
-                                  padding: const EdgeInsets.only(
-                                      right: defaultMargin,
-                                      left: defaultMargin,
-                                      bottom: 16),
-                                  child: OrderListItem(
-                                      transaction: e, itemWidth: listItemWidth),
-                                ))
-                            .toList(),
-                      ),
-                    ],
-                  )),
+                width: double.infinity,
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    CustomTabBar(
+                      titles: ['In Progress', 'Past Orders'],
+                      selectedIndex: selectedIndex,
+                      onTap: (index) {
+                        setState(() {
+                          selectedIndex = index;
+                        });
+                      },
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Column(
+                      children: (selectedIndex == 0 ? inProgress : past)
+                          .map((e) => Padding(
+                                padding: const EdgeInsets.only(
+                                    right: defaultMargin,
+                                    left: defaultMargin,
+                                    bottom: 16),
+                                child: OrderListItem(
+                                    transaction: e, itemWidth: listItemWidth),
+                              ))
+                          .toList(),
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         ],
       );
     }
   }
-
-  void setState(Null Function() param0) {}
-
 }
